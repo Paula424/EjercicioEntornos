4bfa487bd81af6a881cf5bb11f8899bbda504645
osboxes <osboxes@osboxes>
2025-04-22 06:18:45 -0400

    hola

---
+++ src/paquete1/Principal.java
@@ -0,0 +1,12 @@
+package paquete1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+        System.out.println("Primer mensaje del programa");
+        System.out.println("Nuevo mensaje del programa");
+
+
+	}
+
+}
